4cb70cd376a59dea87f754d3faaf65bd0cdcea78
thomaswilde <sedevelopers01@gmail.com>
2013-11-15 15:59:40 +0100

    Update inject.go
    TypeMapper's methods should return a TypeMapper, not a injector

--- inject.go
+++ inject.go
@@ -22,8 +22,8 @@ type Invoker interface {
 }
 
 type TypeMapper interface {
-	Map(interface{}) Injector
-	MapTo(interface{}, interface{}) Injector
+	Map(interface{}) TypeMapper
+	MapTo(interface{}, interface{}) TypeMapper
 	Get(reflect.Type) reflect.Value
 }
 
